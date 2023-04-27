6ae4ca22b7488acad3ec72cb69dd61cf02ef7b38
Ofaehus1 <ofaehus1@gmail.com>
2023-04-26 22:19:08 -0500

    actualizacion

--- config/bd.js
+++ config/bd.js
@@ -3,6 +3,7 @@ import mongoose, { connect } from 'mongoose';
 const conectarBD = () => {
   const urlConexion = String(process.env.MONGO_URI);
   connect(urlConexion)
+  
     .then((con) => {
       console.log(`Conexion establecida con la bd: ${urlConexion}`);
     })
