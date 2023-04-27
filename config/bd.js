a0f67eab3ce57d76089fb94e97f453107159d372
Anderson23j <andersonvalenciaj23@gmail.com>
2023-04-26 22:22:35 -0500

    actualizacion

--- config/bd.js
+++ config/bd.js
@@ -1,13 +1,12 @@
-import mongoose, { connect } from 'mongoose';
+import mongoose, { connect } from "mongoose";
 
 const conectarBD = () => {
   const urlConexion = String(process.env.MONGO_URI);
   connect(urlConexion)
-  
-    .then((con) => {
-      console.log(`Conexion establecida con la bd: ${urlConexion}`);
+    .then(con => {
+      console.log(`Conexión establecida con la base: ${urlConexion}`);
     })
-    .catch((error) => {
+    .catch(error => {
       console.log(error);
     });
 };
