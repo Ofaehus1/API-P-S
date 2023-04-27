f8387e8eb95c28fefa207cb407a42c506e453b9f
Ofaehus1 <ofaehus1@gmail.com>
2023-04-27 13:12:36 -0500

    Jest actualizacion

---
+++ test/app.test.js
@@ -0,0 +1,11 @@
+describe("[APP] Esta es un ejemplo de prueba unitaria", () => {
+    test("Esto debe retornar...", () => {
+  
+      const a = 1
+      const b = 2
+      const c = a + b
+      expect(c).toEqual(3)
+  
+    })
+  })
+  
